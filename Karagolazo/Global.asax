4c32493f0a72e5bd81d1b632f8ffd50a40782666
Ali Kara <mrluckyman@gmail.com>
2016-07-06 21:52:58 +0200

    Project import

---
+++ Karagolazo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Karagolazo.Global" Language="C#" %>
---
+++ Karagolazo/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Karagolazo.Startup))]
+namespace Karagolazo
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
